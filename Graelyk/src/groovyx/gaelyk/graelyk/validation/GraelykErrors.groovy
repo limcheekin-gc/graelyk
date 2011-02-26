8d164de90f516f97c6be58f09f5c23c97ddef7a4
jeremy.r.brown <jeremy.r.brown@0ecd8814-a9ce-f46e-a825-061722498710>
2011-02-26 18:59:56 +0000

    Small changes in GraelykCategory (added getLocalesFromQueryOrCookie() ), StaticResourceHolder, GraelykDomainClass, GraelykErrors

--- Graelyk/src/groovyx/gaelyk/graelyk/validation/GraelykErrors.groovy
+++ Graelyk/src/groovyx/gaelyk/graelyk/validation/GraelykErrors.groovy
@@ -1,10 +1,16 @@
 package groovyx.gaelyk.graelyk.validation
 
 import org.springframework.validation.BeanPropertyBindingResult
+import org.springframework.validation.Errors;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import java.io.Serializable
+import groovyx.gaelyk.graelyk.StaticResourceHolder
 
 class GraelykErrors extends BeanPropertyBindingResult implements Serializable
 {
+	def messageSource
+	
 	public GraelykErrors(Object target, String objectName)
 	{
 		this(target, objectName, true);
@@ -14,4 +20,49 @@ class GraelykErrors extends BeanPropertyBindingResult implements Serializable
 	{
 		super(target, objectName, autoGrowNestedPaths);
 	}
+	
+	public setMessageSource(msgSrc)
+	{
+		messageSource = this.messageSource = msgSrc
+	}
+	
+	public void addError(String messageCode)
+	{
+		addError(messageCode, [] as Object[])
+	}
+	
+    public void addError(String messageCode, Object[] args)
+    {
+        BindingResult result = (BindingResult) this;
+
+        FieldError error = new FieldError(
+                objectName,
+                "",
+                "",
+                false,
+                [] as String[],
+                args,
+                messageCode
+        );
+        ((BindingResult)this).addError(error);
+    }
+    
+    protected String getDefaultMessage(String code)
+    {
+        String defaultMessage;
+
+        try {
+            if(messageSource != null) {
+                defaultMessage = messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
+            }
+            else {
+                defaultMessage = (String)ConstrainedProperty.DEFAULT_MESSAGES.get(code);
+            }
+        }
+        catch(Exception e)
+        {
+            defaultMessage = (String)ConstrainedProperty.DEFAULT_MESSAGES.get(code);
+        }
+        return defaultMessage;
+    }
 }
